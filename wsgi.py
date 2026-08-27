cf5c9bb1eb460d89407841129e256fb8380d3b72
Aarya Linesh Mody <aaryamody0418@gmail.com>
2026-08-27 12:07:23 +0530

    Remove partial embedding limit from WSGI startup

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,10 @@
 """WSGI entry point for production servers such as Gunicorn."""
 
-import os
-
 from app import create_app
 
 
 app = create_app(
     embedding_args={
         "rebuild_embeddings": False,
-        "max_movies": int(os.environ.get("MAX_EMBEDDING_MOVIES", "1000")),
     }
 )
